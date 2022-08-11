e904fd9b29dde828634872f3357c2d39e478f44e
Putoshi <silicopro@gmail.com>
2022-08-12 00:30:14 +0900

    Stylus消す

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {setupStore} from './modules/store/Store.js';
 import { Provider } from 'react-redux';
-// import './index.styl';
 
 
 const store = setupStore();
